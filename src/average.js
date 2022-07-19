6dcbcb8e4645572dfa03acddd40dc32123bdff60
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-19 17:03:47 -0300

    Requisito 1.

--- src/average.js
+++ src/average.js
@@ -11,6 +11,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  let result;
+  if (array.length === 0) { return undefined; }
+
+  for (let list of array) {
+     if (typeof list !== 'number') { return undefined; }
+    sum += list;
+  }
+
+  result = Math.round(sum / array.length);
+  return result;
+};
 
 module.exports = average;
