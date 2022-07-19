cc743a5cebd4c4207aaaa3d1a482e75132c99b1c
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-19 19:19:51 -0300

    Requisito 4.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -22,15 +22,44 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado possui 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
-    // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+  
+  it('Teste se circle retorna undefined', () => {
+    expect(circle()).toEqual(undefined);
+  });
+
+  it('Teste se circle retorna um objeto', () => {
+    let result = { radius: 1, area: 3.14, circumference: 6.28 }
+    expect(circle(1)).toEqual(result);
+  });
+
+  it('Teste se o objeto retornado possui 3 propriedades', () => {
+    let result = { radius: 1, area: 3.14, circumference: 6.28 }
+    expect(circle(1)).toEqual( result );
+  });
+
+
+  it('Teste se a função sem nenhum parâmetro retorna undefined', () => {
+    expect(circle()).toBeUndefined();
+  });
+
+
+  it('Teste se a função retorna o valor da circunferência para um círculo de raio 2', () => {
+    let circum1 = { radius: 2, area: 12.56, circumference: 12.56 }
+    expect(circle(2)).toEqual({ radius: 2, area: 12.56, circumference: 12.56 });
+  });
+
+  it('Teste se a função retorna o valor da circunferência para um círculo de raio 3', () => {
+    let circum1 = { radius: 2, area: 12.56, circumference: 12.56 }
+    expect(circle(3)).toEqual(
+      { radius: 3, area: 28.259999999999998, circumference: 18.84 }
+      );
+  });
+
+
+  it('Teste se a função retorna o valor da circunferência para um círculo de raio 3', () => {
+    let circum1 = { radius: 2, area: 12.56, circumference: 12.56 }
+    expect(circle(3)).toEqual(
+      { radius: 3, area: 28.259999999999998, circumference: 18.84 }
+      );
   });
 });
