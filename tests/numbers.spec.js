df1d339ca9281877749a7159c2515d2355ece2a5
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-20 16:12:08 -0300

    Correções 4.

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -17,18 +17,18 @@ const numbers = require('../src/numbers');
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   
   it('Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true', () => {
-    expect(numbers([1, 2, 3, 4, 5])).toEqual(true);
+    expect(numbers([1, 2, 3, 4, 5])).toBeTruthy();
   });
 
   it('Retorna FALSO com string 3 no centro', () => {
-    expect(numbers([1, 2, '3', 4, 5])).toEqual(false);
+    expect(numbers([1, 2, '3', 4, 5])).toBeFalsy();
   });
 
   it('Retorna FALSO com string a no centro', () => {
-    expect(numbers([1, 'a', 3])).toEqual(false);
+    expect(numbers([1, 'a', 3])).toBeFalsy();
   });
 
   it('Retorna FALSO quando recebe array vazia', () => {
-    expect(numbers([' '])).toEqual(false);
+    expect(numbers([' '])).toBeFalsy();
   });
 });
