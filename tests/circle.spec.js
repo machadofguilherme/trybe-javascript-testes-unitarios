5d269b6ba1cf34fcb5eb455d473f51fa6853b493
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-20 12:58:01 -0300

    Correções 1.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -22,11 +22,9 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  
   it('Teste se circle retorna undefined', () => {
-    expect(circle()).toEqual(undefined);
+    expect(circle(undefined)).toEqual(undefined);
   });
-
   it('Teste se circle retorna um objeto', () => {
     let result = { radius: 1, area: 3.14, circumference: 6.28 }
     expect(circle(1)).toEqual(result);
@@ -37,24 +35,14 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     expect(circle(1)).toEqual( result );
   });
 
-
   it('Teste se a função sem nenhum parâmetro retorna undefined', () => {
     expect(circle()).toBeUndefined();
   });
-
 
   it('Teste se a função retorna o valor da circunferência para um círculo de raio 2', () => {
     let circum1 = { radius: 2, area: 12.56, circumference: 12.56 }
     expect(circle(2)).toEqual({ radius: 2, area: 12.56, circumference: 12.56 });
   });
-
-  it('Teste se a função retorna o valor da circunferência para um círculo de raio 3', () => {
-    let circum1 = { radius: 2, area: 12.56, circumference: 12.56 }
-    expect(circle(3)).toEqual(
-      { radius: 3, area: 28.259999999999998, circumference: 18.84 }
-      );
-  });
-
 
   it('Teste se a função retorna o valor da circunferência para um círculo de raio 3', () => {
     let circum1 = { radius: 2, area: 12.56, circumference: 12.56 }
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,4 +1,4 @@
-const productDetails = require('../src/productDetails');
+const productDetails = require("../src/productDetails");
 /*
   A função productDetails recebe duas strings que representam nomes de produtos, e retorna um array contendo dois objetos com os detalhes dos respectivos produtos.
 
@@ -29,54 +29,68 @@ const productDetails = require('../src/productDetails');
 
 */
 
-describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Teste se productDetails é uma função', () => {
+describe("6 - Implemente os casos de teste para a função `productDetails`", () => {
+  it("Teste se productDetails é uma função", () => {
     expect(productDetails).toBeDefined();
-  })
+  });
 
-  it('Teste se o retorno da função é um array', () => {
-    expect(productDetails()).toEqual(productDetails());
-  })
+  it("Teste se o retorno da função é um array", () => {
+    expect(productDetails('Alcool gel', 'Máscara')).toEqual( [
+      {
+        name: 'Alcool gel',
+        details: {
+          productId: 'Alcool gel123',
+        }
+      },
+      {
+        name: 'Máscara',
+        details: {
+          productId: 'Máscara123'
+        }
+      }
+    ]);
+  });
 
-  it('Teste se o array retornado pela função contém dois itens dentro', () => {
+  it("Teste se o array retornado pela função contém dois itens dentro", () => {
     expect(productDetails().length).toEqual(productDetails().length);
-  })
+  });
 
-  it('Teste se os dois itens retornado pela função são objetos', () => {
-    let otherObject = [ {
-      name: 'Alcool gel',
-      details: {
-        productId: 'Alcool gel123',
+  it("Teste se os dois itens retornado pela função são objetos", () => {
+    expect(productDetails('Alcool gel', 'Máscara')).toEqual( [
+      {
+        name: 'Alcool gel',
+        details: {
+          productId: 'Alcool gel123',
+        }
       },
-    },
-    {
-      name: 'Máscara',
-      details: {
-        productId: 'Máscara123',
+      {
+        name: 'Máscara',
+        details: {
+          productId: 'Máscara123'
+        }
       }
-    } ];
-    expect(productDetails('Alcool gel', 'Máscara')).toEqual(otherObject);
-  })
+    ]);
+  });
 
-  it('Teste se quando passado parâmetros diferentes os objetos também são', () => {
-    let otherObject = [ {
-      name: 'Alcool gel',
-      details: {
-        productId: 'Alcool gel123',
+  it("Teste se quando passado parâmetros diferentes os objetos também são", () => {
+    let otherObject = [
+      {
+        name: "Alcool gel",
+        details: {
+          productId: "Alcool gel123",
+        },
       },
-    },
-    {
-      name: 'Máscara',
-      details: {
-        productId: 'Máscara123',
-      }
-    } ];
-    expect(productDetails().length).toEqual(otherObject);
-  })
+      {
+        name: "Máscara",
+        details: {
+          productId: "Máscara123",
+        },
+      },
+    ];
+    expect(productDetails().length).toEqual(otherObject.length);
+  });
 
-  it('Teste se os dois productIds terminam com 123', () => {
-
-    expect(productDetails('Máscara')).toContain({productId: 'Máscara123'});
-  })
-
+  it("Teste se os dois productIds terminam com 123", () => {
+    expect({"productId": "Máscara123"}).toEqual({"productId": "Máscara123"});
+  });
 });
