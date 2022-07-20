787a981c0c84167f8d269f960563e74ac2ac3f67
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-19 23:07:14 -0300

    Requisito 9.

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -42,14 +42,27 @@ Retorno:
 */
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+  it('Sem nenhum parâmetro retorna undefined.', () => {
+    expect(getCharacter()).toBeUndefined();
+  });
+
+  it('Retorna o objeto correto para o parâmetro `Arya`.', () => {
+    expect(getCharacter('Arya')).toEqual({name: 'Arya Stark', class: 'Rogue', phrases: [ 'Not today', 'A girl has no name.' ]});
+  });
+
+  it('Retorna o objeto correto para o parâmetro `Brienne`.', () => {
+    expect(getCharacter('Brienne')).toEqual({name: 'Brienne Tarth', class: 'Knight', phrases: [ 'Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.' ]});
+  });
+
+  it('Retorna o objeto correto para o parâmetro `Melissandre`.', () => {
+    expect(getCharacter('Melissandre')).toEqual({name: 'Melissandre', class: 'Necromancer', phrases: [ 'Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.' ]});
+  });
+
+  it('Verifica se o parâmetro é case sensitive.', () => {
+    expect(getCharacter('Arya')).toEqual(getCharacter('arya'));
+  });
+
+  it('Verifica se a função retorna undefined caso um nome não seja encontrado.', () => {
+    expect(getCharacter('Guilherme')).toBeUndefined();
   });
 });
